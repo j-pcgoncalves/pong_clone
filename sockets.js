d1689340ce14563049f0d95c875e47e5471cebbf
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2023-12-26 17:09:32 +0000

    functionality to have multiple rooms with socket.io

--- sockets.js
+++ sockets.js
@@ -4,28 +4,34 @@ function listen (io) {
     const pongNamespace = io.of("/pong");
 
     pongNamespace.on("connection", (socket) => {
+        let room;
+
         console.log("A user connected", socket.id);
 
         socket.on("ready", () => {
-            console.log("Player ready", socket.id);
+            room = "Room " + Math.floor(readyPlayerCount / 2);
+            socket.join(room);
+
+            console.log("Player ready", socket.id, room);
 
             readyPlayerCount++;
 
             if (readyPlayerCount % 2 === 0) {
-                pongNamespace.emit("startGame", socket.id);
+                pongNamespace.in(room).emit("startGame", socket.id);
             }
         });
 
         socket.on("paddleMove", (paddleData) => {
-            socket.broadcast.emit("paddleMove", paddleData);
+            socket.to(room).emit("paddleMove", paddleData);
         });
 
         socket.on("ballMove", (balldata) => {
-            socket.broadcast.emit("ballMove", balldata);
+            socket.to(room).emit("ballMove", balldata);
         });
 
         socket.on("disconnect", (reasons) => {
             console.log(`Client ${socket.id} disconnected: ${reasons}`);
+            socket.leave(room);
         });
     });
 }
